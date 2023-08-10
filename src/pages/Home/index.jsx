3e6eaeda774b6cf1611486663b686a3698d47195
unknown <ekopurnama250498@gmail.com>
2023-08-10 10:45:46 +0700

    firsh commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,10 @@ import { Link, useNavigate } from "react-router-dom";
 const HomePages = () => {
   let navigate = useNavigate();
   const [surah, setSurah] = useState();
+  const [search, setSearch] = useState("");
+  const [foundSurah, setFoundSurah] = useState(surah);
+
+  console.log("log", foundSurah);
 
   useEffect(() => {
     const getSurahAll = async () => {
@@ -25,20 +29,20 @@ const HomePages = () => {
   };
 
   const filterBySearch = (event) => {
-    const query = event.target.value;
-    console.log(query);
-    if (query !== "") {
-      const results = surah.filter((surah) => {
-        return surah.nama_latin.toLowerCase().startsWith(keyword.toLowerCase());
+    const keyword = event.target.value;
+    if (keyword !== "") {
+      const results = surah.filter((surat) => {
+        //console.log(surah);
+        return surat.nama_latin.toLowerCase().startsWith(keyword.toLowerCase());
         // Use the toLowerCase() method to make it case-insensitive
       });
-      setSurah(results);
+      setFoundSurah(results);
     } else {
-      setSurah(surah);
+      setFoundSurah(surah);
       // If the text field is empty, show all users
     }
 
-    setSurah(query);
+    setSearch(keyword);
   };
   return (
     <>
@@ -46,27 +50,21 @@ const HomePages = () => {
         <header className="bg-teal-500 shadow">
           <div className="flex w-full object-center justify-center items-center">
             <img
-              width="200"
-              height="200"
-              src="https://quran.kemenag.go.id/assets/images/defaults/ic_quran_logo.png"
-              alt="Your Company"
-            />
-          </div>
-          <div className="flex w-full object-center justify-center items-center">
-            <img
               width="400"
               height="400"
-              src="https://quran.kemenag.go.id/assets/images/defaults/ic_arabic_sentence.png"
+              src="https://www.mushaf.id/sources/img/logo-lg.png"
               alt="Your Company"
             />
           </div>
+
           <div className="sm:col-span-3 m-8">
             <div className="mt-2 m-8 my-8">
               <input
                 type="text"
                 name="search"
+                value={search}
                 id="search-box"
-                onChange={() => filterBySearch}
+                onChange={filterBySearch}
                 className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="cari surat yang ingin anda baca? (Contoh : Al-Fatihah, An-Nas"
               />
@@ -77,45 +75,77 @@ const HomePages = () => {
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           <div>
             <div className="grid grid-cols-3 gap-3">
-              {surah && surah.length > 0 ? (
-                surah.map((Surah, index) => (
-                  <div key={Surah?.nomor}>
-                    <a
-                      onClick={() => {
-                        getSuratALQuran(Surah?.nomor);
-                      }}
-                    >
-                      <div
-                        className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200"
-                        id="surah-list"
+              {foundSurah && foundSurah.length > 0
+                ? foundSurah?.map((Surah, index) => (
+                    <div key={Surah?.nomor}>
+                      <a
+                        onClick={() => {
+                          getSuratALQuran(Surah?.nomor);
+                        }}
                       >
-                        <div className="grid grid-cols-2 gap-3 m-2">
-                          <div className="margin-right-6">
-                            <p className="font-semibold text-indigo-900">
-                              {index + 1 + ". "} {Surah?.nama_latin} <br />{" "}
-                            </p>
-                          </div>
-                          <div>
-                            <div style={{ marginLeft: "60px" }}>
+                        <div
+                          className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200"
+                          id="surah-list"
+                        >
+                          <div className="grid grid-cols-2 gap-3 m-2">
+                            <div className="margin-right-6">
                               <p className="font-semibold text-indigo-900">
-                                {Surah?.nama}
+                                {index + 1 + ". "} {Surah?.nama_latin} <br />{" "}
                               </p>
                             </div>
+                            <div>
+                              <div style={{ marginLeft: "60px" }}>
+                                <p className="font-semibold text-indigo-900">
+                                  {Surah?.nama}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="flex w-full object-center justify-center items-center">
+                            <p className="text-teal-700">
+                              {Surah?.tempat_turun} :{" "}
+                              {Surah?.jumlah_ayat + " " + "Ayat"}{" "}
+                            </p>
                           </div>
                         </div>
-                        <div className="flex w-full object-center justify-center items-center">
-                          <p className="text-teal-700">
-                            {Surah?.tempat_turun} :{" "}
-                            {Surah?.jumlah_ayat + " " + "Ayat"}{" "}
-                          </p>
+                      </a>
+                    </div>
+                  ))
+                : surah?.map((Surah, index) => (
+                    <div key={Surah?.nomor}>
+                      <a
+                        onClick={() => {
+                          getSuratALQuran(Surah?.nomor);
+                        }}
+                      >
+                        <div
+                          className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200"
+                          id="surah-list"
+                        >
+                          <div className="grid grid-cols-2 gap-3 m-2">
+                            <div className="margin-right-6">
+                              <p className="font-semibold text-indigo-900">
+                                {index + 1 + ". "} {Surah?.nama_latin} <br />{" "}
+                              </p>
+                            </div>
+                            <div>
+                              <div style={{ marginLeft: "60px" }}>
+                                <p className="font-semibold text-indigo-900">
+                                  {Surah?.nama}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="flex w-full object-center justify-center items-center">
+                            <p className="text-teal-700">
+                              {Surah?.tempat_turun} :{" "}
+                              {Surah?.jumlah_ayat + " " + "Ayat"}{" "}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </a>
-                  </div>
-                ))
-              ) : (
-                <h1>No results found!</h1>
-              )}
+                      </a>
+                    </div>
+                  ))}
             </div>
           </div>
         </div>
